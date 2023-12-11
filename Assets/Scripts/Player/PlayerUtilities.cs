21df46f225c1b0e8f1a7ddbafa613f3749fcc8c6
Han <79167052+hannert@users.noreply.github.com>
2023-12-10 20:53:49 -0500

    Materials for Obstacles
    Added more materials to differentiate between grounds and walls.

--- Assets/Scripts/Player/PlayerUtilities.cs
+++ Assets/Scripts/Player/PlayerUtilities.cs
@@ -17,14 +17,14 @@ public class PlayerUtilities
         Array.Clear(groundColliders, 0, groundColliders.Length);
 
         //We dont need point1 right now, but will be used if we need to use a capsule cast to reflect the players capsule collider size
-        var localPoint1 = playerCap.center - Vector3.down * (playerCap.height / 2 - playerCap.radius);
+        var localPoint1 = playerCap.center - Vector3.down * (playerCap.height / 2 - (playerCap.radius - 0.2f));
         // Have the point SLIGHTLY more UNDER the player collider
-        var localPoint2 = playerCap.center + Vector3.down * (playerCap.height / 2 - playerCap.radius) * 1.1f; // Above point
+        var localPoint2 = playerCap.center + Vector3.down * (playerCap.height / 2 - (playerCap.radius - 0.2f)) * 1.1f; // Above point
 
         var point1 = playerCap.transform.TransformPoint(localPoint1);
         var point2 = playerCap.transform.TransformPoint(localPoint2);
 
-        DebugExtension.DebugWireSphere(point2, playerCap.radius, Time.fixedDeltaTime);
+        DebugExtension.DebugWireSphere(point2, playerCap.radius - 0.2f, Time.fixedDeltaTime);
         int numColliders = Physics.OverlapSphereNonAlloc(point2, playerCap.radius, groundColliders, LayerMask.GetMask("Ground"));
 
         return numColliders;
