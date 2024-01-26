a226ad26d7fc22e6766f85b31aecdf0aed08e079
Han <79167052+hannert@users.noreply.github.com>
2024-01-26 16:55:33 -0500

    Game Objects
    Updated the testing area for the game with new objects

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    public bool goBackwards;
     public float speed;
     Rigidbody objectRb;
+    BoxCollider objectCollider;
 
     void Start()
     {
         objectRb = GetComponent<Rigidbody>();
-
+        objectCollider = GetComponent<BoxCollider>();
     }
 
     void FixedUpdate()
     {
-        objectRb.AddForce(Vector3.forward * Time.fixedDeltaTime * speed, ForceMode.Force);
+        if (goBackwards)
+        {
+            objectRb.AddForce(Vector3.back * Time.fixedDeltaTime * speed, ForceMode.VelocityChange);
+        }
+        else
+        {
+            objectRb.AddForce(Vector3.forward * Time.fixedDeltaTime * speed, ForceMode.VelocityChange);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.GetMask("Wall"))
+        {
+            goBackwards = !goBackwards;
+        }
     }
 }
