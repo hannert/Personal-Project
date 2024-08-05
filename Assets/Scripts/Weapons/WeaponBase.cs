6c6c4bd34f2019c42922223c7e78f3e7005c20cf
Han <79167052+hannert@users.noreply.github.com>
2024-08-05 19:29:38 -0400

    Slash VFX update
    Slash objects should be a child of the slashing position of the weapon so it can follow the rotation of the weapon

--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -5,14 +5,32 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.VFX;
 
+/// <summary>
+/// The logic for weapons in the game
+/// </summary>
 public class WeaponBase : MonoBehaviour
 {
+    /// <summary>
+    /// Base damage the weapon deals before any modifiers
+    /// </summary>
     public float baseDamage;
 
+    /// <summary>
+    /// The scriptable object holding the moveset for this weapon
+    /// </summary>
     public CombatMovesetObject movesetObject;
 
+    /// <summary>
+    /// The moveset class for the wepaon
+    /// </summary>
     private CombatMoveset moveset;
 
+    /// <summary>
+    /// List of colliders hit with the weapon, used to deal damage once per move
+    /// </summary>
+    private List<Collider> hitEntities = new List<Collider>();
+
+    #region Current Variables
     /// <summary>
     /// Index of which combo of the moveset we are on
     /// </summary>
@@ -23,34 +41,89 @@ public class WeaponBase : MonoBehaviour
     /// </summary>
     private int currentMoveIndex;
 
+    /// <summary>
+    /// The current CombatCombo we are on
+    /// </summary>
     private CombatCombo currentCombo { get; set; }
-    private CombatMove currentMove { get; set;}
-    private CombatBaseObject currentAction { get; set; }
-
-    private VisualEffectAsset currentActionSlashVFX { get; set; }
-
-    private float currentActionSlashDelay { get; set; }
+    public CombatBindsEnum GetCurrentComboKey() {
+        return currentCombo.type;
+    }
 
     /// <summary>
-    /// List of colliders hit with the weapon, used to deal damage once per move
+    /// The current CombatMove we are on
     /// </summary>
-    private List<Collider> hitEntities = new List<Collider>();
+    private CombatMove currentMove { get; set;}
 
-    public GameObject slashPosition;
+    /// <summary>
+    /// The current CombatAction we are on
+    /// </summary>
+    private CombatBaseObject currentAction { get; set; }
+    public CombatBaseObject GetCurrentAction() {
+        return currentAction;
+    }
+
+    #endregion
+
+    #region VFX
+
+    /// <summary>
+    /// Which visual effect slash this action is associated with
+    /// </summary>
+    private VisualEffectAsset currentActionSlashVFX { get; set; }
+
+    /// <summary>
+    /// How long to wait to play the slash effect
+    /// </summary>
+    private float currentActionSlashDelay { get; set; }
+    public float GetSlashDelay() {
+        return currentActionSlashDelay;
+    }
+
+    /// <summary>
+    /// The parent GameObject the slash is attached to on the weapon
+    /// </summary>
     public GameObject slashObject;
 
+    /// <summary>
+    /// The visual effect component in the slash GameObject
+    /// </summary>
     private VisualEffect slashEffect;
+    
+    /// <summary>
+    /// Apply any rotation adjustments and play the slash effect currently loaded
+    /// </summary>
+    public void PlaySlashEffect() {
+
+        // Apply the rotation offset from the current CombatMove
+        Vector3 offset = currentMove.GetSlashRotation();
+        if (offset != Vector3.zero){
+            Quaternion newRotation = slashObject.transform.rotation;
+
+            newRotation.x += offset.x;
+            newRotation.y += offset.y;
+            newRotation.z += offset.z;
+
+            slashObject.transform.rotation = newRotation;
+        }        
+
+        slashEffect.Play();
+    }
+    #endregion
+
+    #region Misc
+    public CombatLink[] GetCombatLinks() {
+        return currentCombo.combo[currentMoveIndex].linkableActions;
+    }
 
     void Awake() {
         moveset = movesetObject.moveset;
         slashEffect = slashObject.GetComponentInChildren<VisualEffect>();
     }
 
-
     /// <summary>
-    /// 
+    /// Jumps to specified MOVE of a specific COMBO via LINK data
     /// </summary>
-    /// <param name="link"></param>
+    /// <param name="link">The CombatLink object containing jump information</param>
     /// <returns></returns>
     public void LinkToNewCombo(CombatLink link) {
         Debug.Log("Linking new combo" + link.comboIndex.ToString() + " " + link.moveIndex.ToString());
@@ -88,33 +161,11 @@ public class WeaponBase : MonoBehaviour
         return false;
     }
 
-
-    public float GetSlashDelay() {
-        return currentActionSlashDelay;
-    }
-    public void PlaySlashEffect() {
-        slashObject.transform.position = slashPosition.transform.position;
-        slashEffect.Play();
-    }
-
-    public CombatBaseObject GetCurrentAction() {
-        return currentAction;
-    }
-    public CombatBindsEnum GetCurrentComboKey() {
-        return currentCombo.type;
-    }
-
-
-    public CombatLink[] GetCombatLinks() {
-        return currentCombo.combo[currentMoveIndex].linkableActions;
-    }
-
-    // public void ResetCombo() {
-    //     currentMoveIndex = 0;
-    //     currentMove = moveset[0];
-    //     hitEntities.Clear();
-    // }
-
+    /// <summary>
+    /// Prepares the weapons internal data variables to the start of the combo associated with key
+    /// </summary>
+    /// <param name="key">The keycode entered</param>
+    /// <returns>The combo associated with the keycode</returns>
     public bool Prepare(KeyCode key) {
         // Clear hit colliders array since we are going to a new move
         hitEntities.Clear();
@@ -150,7 +201,5 @@ public class WeaponBase : MonoBehaviour
         return true;
     }
 
-
-
-
+    #endregion
 }
